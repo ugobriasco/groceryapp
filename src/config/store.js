bd0a140e528095a28734b9218808649d38938651
Ugo Briasco <ugo.briasco@gmail.com>
2017-08-28 11:13:54 +0200

    async store - now the app preserve its state offline

--- src/config/store.js
+++ src/config/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { AsyncStorage } from 'react-native';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
 import reducers from '../reducers';
 import rootSaga from './sagas';
@@ -12,7 +14,13 @@ if(process.env.NODE_ENV === 'development'){
 	middleware.push(logger);
 }
 
-const store = createStore(reducers, applyMiddleware(...middleware));
+const store = createStore(
+	reducers, 
+	applyMiddleware(...middleware),
+	autoRehydrate()
+);
+
+persistStore(store, { storage: AsyncStorage });
 sagaMiddleware.run(rootSaga);
 
 export default store;
