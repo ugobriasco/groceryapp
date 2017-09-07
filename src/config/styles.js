d246b234460872ef1c40309334d154565252c838
Ugo Briasco <ugo.briasco@gmail.com>
2017-09-07 22:49:16 +0200

    setting up unit testing

---
+++ src/config/styles.js
@@ -0,0 +1,15 @@
+import Estylesheet from 'react-native-extended-stylesheet';
+
+export default () => {
+	Estylesheet.build({
+		$brandingBackground: '#18bc9c',
+		$primaryBackground: '#FFFF',
+		$dangerBackground: '#ea5a4d',
+		$successBackground: '#18bc9c',
+		$primaryBoxColor: '#EFF0F1',
+		$primaryTextColor: '#777777',
+		$border: '#77777b',
+
+	});
+
+};
